20ef5e7a15fb56d186a9d59acf0ec9217c13ff43
Hyeok_Choi <love72huk@naver.com>
2024-03-19 14:25:53 +0900

    initial project

---
+++ src/test/java/com/romanticpipe/reviewcanvas/ReviewCanvasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romanticpipe.reviewcanvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewCanvasBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
