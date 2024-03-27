1f2dd2a7f2db38c2fbf98dad2dab36c70cdcde49
Hyeok_Choi <love72huk@naver.com>
2024-03-27 21:04:55 +0900

    feat(RVCV-56): api response dto를 만든다

---
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/common/dto/ErrorResponse.java
@@ -0,0 +1,43 @@
+package com.romanticpipe.reviewcanvas.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.romanticpipe.reviewcanvas.exception.ErrorCode;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+@Getter
+@Schema(description = "성공 Response")
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ErrorResponse {
+
+	@Schema(description = "성공 여부", defaultValue = "false")
+	private final boolean success = false;
+	private final ErrorInfo data;
+
+
+	public static ErrorResponse of(ErrorCode errorCode) {
+		return new ErrorResponse(new ErrorInfo(errorCode.getCode(), errorCode.getMessage(), null));
+	}
+
+	public static ErrorResponse of(ErrorCode errorCode, List<ValidationError> validationErrorList) {
+		return new ErrorResponse(new ErrorInfo(errorCode.getCode(), errorCode.getMessage(), validationErrorList));
+	}
+
+	public record ValidationError(String field, String message) {
+
+		public static ValidationError of(final FieldError fieldError) {
+			return new ValidationError(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+	}
+
+	private record ErrorInfo(String code,
+	                         String message,
+	                         @JsonInclude(JsonInclude.Include.NON_EMPTY)
+	                         List<ValidationError> errors) {
+	}
+}
---
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/common/dto/SuccessResponse.java
@@ -0,0 +1,34 @@
+package com.romanticpipe.reviewcanvas.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@Schema(description = "성공 Response")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SuccessResponse<T> {
+
+	@Schema(description = "성공 여부", defaultValue = "true")
+	private final boolean success = true;
+
+	@JsonInclude(Include.NON_NULL)
+	private T data;
+
+	public static <T> SuccessResponse<T> of(T data) {
+		SuccessResponse<T> SuccessResponse = new SuccessResponse<>();
+
+		SuccessResponse.data = data;
+
+		return SuccessResponse;
+	}
+
+	public ResponseEntity<SuccessResponse<T>> asHttp(HttpStatus httpStatus) {
+		return ResponseEntity.status(httpStatus).body(this);
+	}
+}
