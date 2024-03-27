c98cf5ff9579d6e5da41894f48b1f855759d2508
Hyeok_Choi <love72huk@naver.com>
2024-03-27 21:04:21 +0900

    feat(RVCV-56): SwaggerConfig를 설정한다

---
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/common/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.romanticpipe.reviewcanvas.common.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+	info = @Info(title = "리뷰캔버스 API 문서",
+		description = "[Regacy] 리뷰캔버스 프로젝트의 API 문서입니다.",
+		version = "v1"),
+	servers = {
+		@Server(url = "https://review-canvas-dev.com", description = "테스트 서버"),
+		@Server(url = "https://review-canvas.com", description = "운영 서버")
+	}
+)
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public GroupedOpenApi allOpenApi() {
+		String[] paths = {"/**"};
+
+		return GroupedOpenApi
+			.builder()
+			.group("모든 API")
+			.pathsToMatch(paths)
+			.build();
+	}
+
+}
