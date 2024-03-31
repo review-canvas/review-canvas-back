3ddac176c0cb2485a0d3f943c7500edc413402ff
JunseoHo <junseoho1029@gmail.com>
2024-03-31 16:18:04 +0900

    feat(RVCV-70) : 리뷰를 수정한다.

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReviewApi.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReviewApi.java
@@ -3,10 +3,14 @@ package com.romanticpipe.reviewcanvas.domain.review.presentation.v1;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.romanticpipe.reviewcanvas.common.dto.SuccessResponse;
+import com.romanticpipe.reviewcanvas.domain.review.application.usecase.request.UpdateReviewRequest;
 import com.romanticpipe.reviewcanvas.domain.review.application.usecase.response.GetReviewResponse;
+import com.romanticpipe.reviewcanvas.domain.review.application.usecase.response.UpdateReviewResponse;
 import com.romanticpipe.reviewcanvas.dto.PageResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,44 +21,30 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Review", description = "리뷰 API")
 interface ReviewApi {
 
-	@Operation(summary = "상품 리뷰 조회 API", description = "특정 상품의 리뷰를 조회한다.")
+	@Operation(summary = "리뷰 조회 API", description = "특정 상품의 리뷰를 조회한다.")
 	@ApiResponses(value = {
 		@ApiResponse(
 			responseCode = "200",
-			description = "성공적으로 상품의 리뷰 조회가 완료되었습니다.")
+			description = "성공적으로 리뷰 조회가 완료되었습니다.")
 	})
 	@GetMapping("/products/{productId}/reviews")
-	ResponseEntity<SuccessResponse<PageResponse<GetReviewResponse>>> getReviewsByProductId(
+	ResponseEntity<SuccessResponse<PageResponse<GetReviewResponse>>> getReviews(
 		@PathVariable("productId") String productId,
 		@RequestParam(value = "size", required = false, defaultValue = "20") int size,
 		@RequestParam(value = "page", required = false, defaultValue = "0") int page,
 		@RequestParam(name = "direction", required = false, defaultValue = "DESC") String direction
 	);
 
-	@Operation(summary = "사용자 리뷰 조회 API", description = "특정 사용자의 리뷰를 조회한다.")
+	@Operation(summary = "리뷰 수정 API", description = "리뷰를 수정한다.")
 	@ApiResponses(value = {
 		@ApiResponse(
 			responseCode = "200",
-			description = "성공적으로 사용자의 리뷰 조회가 완료되었습니다.")
+			description = "성공적으로 리뷰 수정이 완료되었습니다.")
 	})
-	@GetMapping("/users/{userId}/reviews")
-	ResponseEntity<SuccessResponse<PageResponse<GetReviewResponse>>> getReviewsByUserId(
-		@PathVariable("userId") String userId,
-		@RequestParam(value = "size", required = false, defaultValue = "20") int size,
-		@RequestParam(value = "page", required = false, defaultValue = "0") int page,
-		@RequestParam(name = "direction", required = false, defaultValue = "DESC") String direction
+	@PutMapping("/reviews/{reviewId}")
+	ResponseEntity<SuccessResponse<UpdateReviewResponse>> updateReview(
+		@PathVariable("reviewId") long reviewId,
+		@RequestBody UpdateReviewRequest request
 	);
 
-	@Operation(summary = "리뷰 수정 API", description = "특정 상품의 리뷰를 수정한다.")
-	@ApiResponses(value = {
-		@ApiResponse(
-			responseCode = "200",
-			description = "성공적으로 리뷰 조회가 완료되었습니다.")
-	})
-	@GetMapping("/reviews/{reviewId}")
-	ResponseEntity<SuccessResponse<PageResponse<GetReviewResponse>>> updateReview(
-		@PathVariable("reviewId") String productId,
-
-		@Schema(description = "ASC, DESC 가능") Direction direction
-	);
 }
--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReviewController.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReviewController.java
@@ -4,13 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.romanticpipe.reviewcanvas.common.dto.SuccessResponse;
 import com.romanticpipe.reviewcanvas.domain.review.application.usecase.ReviewUseCase;
+import com.romanticpipe.reviewcanvas.domain.review.application.usecase.request.UpdateReviewRequest;
 import com.romanticpipe.reviewcanvas.domain.review.application.usecase.response.GetReviewResponse;
+import com.romanticpipe.reviewcanvas.domain.review.application.usecase.response.UpdateReviewResponse;
 import com.romanticpipe.reviewcanvas.dto.PageResponse;
 import com.romanticpipe.reviewcanvas.dto.PageableRequest;
 
@@ -25,26 +29,26 @@ class ReviewController implements ReviewApi {
 
 	@Override
 	@GetMapping("/products/{productId}/reviews")
-	public ResponseEntity<SuccessResponse<PageResponse<GetReviewResponse>>> getReviewsByProductId(
+	public ResponseEntity<SuccessResponse<PageResponse<GetReviewResponse>>> getReviews(
 		@PathVariable("productId") String productId,
 		@RequestParam(value = "size", required = false, defaultValue = "10") int size,
 		@RequestParam(value = "page", required = false, defaultValue = "0") int page,
 		@RequestParam(name = "direction", required = false, defaultValue = "DESC") String direction
 	) {
 		return SuccessResponse.of(
-			reviewUseCase.getReviewsByProductId(productId, PageableRequest.of(page, size, direction))
+			reviewUseCase.getReviews(productId, PageableRequest.of(page, size, direction))
 		).asHttp(HttpStatus.OK);
 	}
 
 	@Override
-	@GetMapping("/users/{userId}/reviews")
-	public ResponseEntity<SuccessResponse<PageResponse<GetReviewResponse>>> getReviewsByUserId(
-		@PathVariable("userId") String userId,
-		@RequestParam(value = "size", required = false, defaultValue = "20") int size,
-		@RequestParam(value = "page", required = false, defaultValue = "0") int page,
-		@RequestParam(name = "direction", required = false, defaultValue = "DESC") String direction) {
-		return SuccessResponse.of(
-			reviewUseCase.getReviewsByUserId(userId, PageableRequest.of(page, size, direction))
-		).asHttp(HttpStatus.OK);
+	@PutMapping("/reviews/{reviewId}")
+	public ResponseEntity<SuccessResponse<UpdateReviewResponse>> updateReview(
+		@PathVariable("reviewId") long reviewId,
+		@RequestBody UpdateReviewRequest request
+	) {
+		UpdateReviewResponse response = reviewUseCase.updateReview(reviewId, request.score(), request.content());
+		return SuccessResponse.of(response)
+			.asHttp(HttpStatus.OK);
 	}
+
 }
