df9d0154f93ee5cf84137e54643c527b70f6faa1
Gyulguma <ybg653924@gmail.com>
2024-03-31 19:41:01 +0900

    feat(RVCV-98): ReviewItem의 Entity 클래스 생성

---
+++ domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/domain/ReviewItem.java
@@ -0,0 +1,44 @@
+package com.romanticpipe.reviewcanvas.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class ReviewItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "review_item_id")
+	private Long id;
+
+	private Boolean title;
+	private Boolean author;
+	private Boolean point;
+	private Boolean media;
+	private Boolean content;
+	private Boolean createdAt;
+	private Boolean updatedAt;
+
+	private Long shopAdminId;
+
+	@Builder
+	public ReviewItem(Boolean title, Boolean author, Boolean point, Boolean media, Boolean content, Boolean createdAt,
+		Boolean updatedAt, Long shopAdminId) {
+		this.title = title;
+		this.author = author;
+		this.point = point;
+		this.media = media;
+		this.content = content;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.shopAdminId = shopAdminId;
+	}
+
+}
