7785a00f7e70059ae0ba804c4f3347ffaeb99cc5
gyuchangShim <shim17@mju.ac.kr>
2024-03-31 11:05:54 +0900

    feat(RVCV-73): 승인 대기 중인 리뷰 반환 함수 작성

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/ReviewUseCaseImpl.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/ReviewUseCaseImpl.java
@@ -1,12 +1,15 @@
 package com.romanticpipe.reviewcanvas.domain.review.application.usecase;
 
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.romanticpipe.reviewcanvas.domain.review.application.usecase.response.GetReviewResponse;
+import com.romanticpipe.reviewcanvas.domain.review.application.usecase.response.GetWaitingReviewResponse;
 import com.romanticpipe.reviewcanvas.dto.PageResponse;
 import com.romanticpipe.reviewcanvas.dto.PageableRequest;
 import com.romanticpipe.reviewcanvas.service.ReviewReader;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -19,6 +22,14 @@ class ReviewUseCaseImpl implements ReviewUseCase {
 	public PageResponse<GetReviewResponse> getReviews(String productId, PageableRequest pageableRequest) {
 		return reviewReader.findByProductId(productId, pageableRequest)
 			.map(GetReviewResponse::from);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public PageResponse<GetWaitingReviewResponse> getWaitingReviews(String productId,
+		PageableRequest pageableRequest) {
+		return reviewReader.findAllByisChecked(productId, pageableRequest)
+			.map(GetWaitingReviewResponse::from);
 	}
 
 }
--- domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/service/ReviewReader.java
+++ domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/service/ReviewReader.java
@@ -1,13 +1,15 @@
 package com.romanticpipe.reviewcanvas.service;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
 import com.romanticpipe.reviewcanvas.domain.Review;
 import com.romanticpipe.reviewcanvas.dto.PageResponse;
 import com.romanticpipe.reviewcanvas.dto.PageableRequest;
 import com.romanticpipe.reviewcanvas.repository.ReviewRepository;
 import com.romanticpipe.reviewcanvas.util.PageableUtils;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -19,4 +21,10 @@ public class ReviewReader {
 		Pageable pageable = PageableUtils.toPageable(pageableRequest);
 		return PageableUtils.toPageResponse(reviewRepository.findAllByProductId(productId, pageable));
 	}
+
+	public PageResponse<Review> findAllByisChecked(String productId, PageableRequest pageableRequest) {
+		Pageable pageable = PageableUtils.toPageable(pageableRequest);
+		return PageableUtils.toPageResponse(reviewRepository.findAllByisChecked(false));
+	}
+
 }
