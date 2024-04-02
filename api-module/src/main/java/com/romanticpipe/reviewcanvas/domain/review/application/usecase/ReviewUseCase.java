f90742cdf681c1424d2e7ed660c1575b59742aa6
JunseoHo <112461939+JunseoHo@users.noreply.github.com>
2024-04-02 16:32:55 +0900

    refactor(RVCV-114) : 기존 커밋을 새로운 develop 형상으로 rebase한다.

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/ReviewUseCase.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/ReviewUseCase.java
@@ -1,7 +1,5 @@
 package com.romanticpipe.reviewcanvas.domain.review.application.usecase;
 
-import com.romanticpipe.reviewcanvas.domain.review.application.usecase.request.CreateReviewRequest;
-import com.romanticpipe.reviewcanvas.domain.review.application.usecase.request.UpdateReviewRequest;
 import com.romanticpipe.reviewcanvas.domain.review.application.usecase.response.GetReviewResponse;
 import com.romanticpipe.reviewcanvas.dto.PageResponse;
 import com.romanticpipe.reviewcanvas.dto.PageableRequest;
@@ -11,7 +9,4 @@ public interface ReviewUseCase {
 
 	PageResponse<GetReviewResponse> getReviewsByUserId(String userId, PageableRequest pageableRequest);
 
-	void createReview(String productId, CreateReviewRequest createReviewRequest);
-
-	void updateReview(long reviewId, UpdateReviewRequest updateReviewRequest);
 }
--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReviewApi.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReviewApi.java
@@ -1,8 +1,6 @@
 package com.romanticpipe.reviewcanvas.domain.review.presentation.v1;
 
 import com.romanticpipe.reviewcanvas.common.dto.SuccessResponse;
-import com.romanticpipe.reviewcanvas.domain.review.application.usecase.request.CreateReviewRequest;
-import com.romanticpipe.reviewcanvas.domain.review.application.usecase.request.UpdateReviewRequest;
 import com.romanticpipe.reviewcanvas.domain.review.application.usecase.response.GetReviewResponse;
 import com.romanticpipe.reviewcanvas.dto.PageResponse;
 import com.romanticpipe.reviewcanvas.enumeration.Direction;
@@ -10,14 +8,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "Review", description = "리뷰 API")
@@ -32,37 +26,13 @@ interface ReviewApi {
 	@GetMapping("/products/{productId}/reviews")
 	ResponseEntity<SuccessResponse<PageResponse<GetReviewResponse>>> getReviewsByProductId(
 		@PathVariable("productId") String productId,
-		@RequestParam(value = "size", required = false, defaultValue = "20") int size,
+		@RequestParam(value = "size", required = false, defaultValue = "10") int size,
 		@RequestParam(value = "page", required = false, defaultValue = "0") int page,
-		@RequestParam(name = "direction", required = false, defaultValue = "DESC") Direction direction
+		@RequestParam(name = "direction", required = false, defaultValue = "DESC")
+		@Schema(description = "ASC, DESC 가능") Direction direction
 	);
 
-	@Operation(summary = "상품 리뷰 생성 API", description = "특정 상품의 리뷰를 생성한다.")
-	@ApiResponses(value = {
-		@ApiResponse(
-			responseCode = "200",
-			description = "성공적으로 상품의 리뷰 생성이 완료되었습니다.")
-	})
-	@PostMapping("/products/{productId}/reviews")
-	ResponseEntity<SuccessResponse<Void>> createReview(
-		@PathVariable("productId") String productId,
-		@RequestBody CreateReviewRequest createReviewRequest
-	);
-
-	@Operation(summary = "리뷰 수정 API", description = "특정 상품의 리뷰를 수정한다.")
-	@ApiResponses(value = {
-		@ApiResponse(
-			responseCode = "200",
-			description = "성공적으로 리뷰 조회가 완료되었습니다.")
-	})
-	@PatchMapping("/reviews/{reviewId}")
-	ResponseEntity<SuccessResponse<Void>> updateReview(
-		@PathVariable("reviewId") long productId,
-		@RequestBody UpdateReviewRequest updateReviewRequest
-	);
-
-	@Operation(summary = "사용자 리뷰 조회 API", description = "특정 사용자의 리뷰를 조회한다.",
-		security = @SecurityRequirement(name = "Bearer Authentication"))
+	@Operation(summary = "사용자 리뷰 조회 API", description = "특정 사용자의 리뷰를 조회한다.")
 	@ApiResponses(value = {
 		@ApiResponse(
 			responseCode = "200",
