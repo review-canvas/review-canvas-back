7227ac23e6624980c3e2a9f8de5ed50a6c09d134
wodnd0131 <wodnd0131@naver.com>
2024-04-02 14:05:02 +0900

    feat(RVCV-108): SuperAdmin 엔티티를 추가한다.

---
+++ domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/domain/SuperAdmin.java
@@ -0,0 +1,36 @@
+package com.romanticpipe.reviewcanvas.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class SuperAdmin implements AdminInterface {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "super_admin_id")
+	private Long id;
+
+	private String email;
+	private String password;
+	private String refreshToken;
+
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	@Builder
+	public SuperAdmin(String email, String password) {
+		this.email = email;
+		this.password = password;
+		this.role = Role.SUPER;
+	}
+}
---
+++ domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/service/SuperAdminValidator.java
@@ -0,0 +1,26 @@
+package com.romanticpipe.reviewcanvas.service;
+
+import org.springframework.stereotype.Service;
+
+import com.romanticpipe.reviewcanvas.domain.SuperAdmin;
+import com.romanticpipe.reviewcanvas.exception.BusinessException;
+import com.romanticpipe.reviewcanvas.exception.ShopAdminErrorCode;
+import com.romanticpipe.reviewcanvas.repository.SuperAdminRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SuperAdminValidator {
+	private final SuperAdminRepository superAdminRepository;
+
+	public SuperAdmin findByEmail(String email) {
+		return this.superAdminRepository.findByEmail(email)
+			.orElseThrow(() -> new BusinessException(ShopAdminErrorCode.SHOP_ADMIN_NOT_FOUND));
+	}
+
+	public SuperAdmin findById(Long shopAdminId) {
+		return this.superAdminRepository.findById(shopAdminId)
+			.orElseThrow(() -> new BusinessException(ShopAdminErrorCode.SHOP_ADMIN_NOT_FOUND));
+	}
+}
