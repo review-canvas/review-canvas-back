53ed1d12e11f7c46042da4047d3dd165b58c977f
Hyeok_Choi <love72huk@naver.com>
2024-04-07 23:50:00 +0900

    fix(RVCV-121): Direction Converter 로직을 수정한다

--- common-module/src/main/java/com/romanticpipe/reviewcanvas/enumeration/Direction.java
+++ common-module/src/main/java/com/romanticpipe/reviewcanvas/enumeration/Direction.java
@@ -1,5 +1,17 @@
 package com.romanticpipe.reviewcanvas.enumeration;
 
+import java.util.Arrays;
+
 public enum Direction {
 	ASC, DESC, NONE;
+
+	public static Direction of(String direction) {
+		if (direction == null) {
+			return NONE;
+		}
+		return Arrays.stream(Direction.values())
+			.filter(d -> d.name().equals(direction.toUpperCase()))
+			.findFirst()
+			.orElseGet(() -> NONE);
+	}
 }
