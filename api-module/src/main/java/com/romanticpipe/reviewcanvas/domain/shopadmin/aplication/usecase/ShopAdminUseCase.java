71184f6d80605231e83ff41ff8f5f2cb5d622318
wodnd0131 <wodnd0131@naver.com>
2024-04-09 16:29:33 +0900

    feat(RVCV-88): Shopadmin 로그아웃 api를 생성한다.

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/shopadmin/aplication/usecase/ShopAdminUseCase.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/shopadmin/aplication/usecase/ShopAdminUseCase.java
@@ -1,17 +1,20 @@
 package com.romanticpipe.reviewcanvas.domain.shopadmin.aplication.usecase;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.romanticpipe.reviewcanvas.domain.AdminInterface;
 import com.romanticpipe.reviewcanvas.domain.Role;
 import com.romanticpipe.reviewcanvas.domain.shopadmin.aplication.usecase.request.SignUpRequest;
 import com.romanticpipe.reviewcanvas.domain.shopadmin.aplication.usecase.response.CheckLoginResponse;
 import com.romanticpipe.reviewcanvas.domain.shopadmin.aplication.usecase.response.LoginResponse;
-import org.springframework.web.multipart.MultipartFile;
 
 public interface ShopAdminUseCase {
 
 	LoginResponse login(String email, String password, Role role);
 
 	void signUp(SignUpRequest signUpRequest, MultipartFile logoImage);
+
+	void logout(AdminInterface admin);
 
 	CheckLoginResponse checkLoginForAdmin(AdminInterface admin);
 }
