95ffe1cb5791d9e1e297f63ee325dc5cb3262b41
Hyeok_Choi <love72huk@naver.com>
2024-04-11 01:38:17 +0900

    fix(RVCV-121): WebClient 예외를 삭제한다

--- client-module/src/main/java/com/romanticpipe/reviewcanvas/exception/WebClientException.java
+++
@@ -1,8 +0,0 @@
-package com.romanticpipe.reviewcanvas.exception;
-
-public class WebClientException extends BusinessException {
-	public WebClientException(ErrorCode errorCode, String body) {
-		super(errorCode, "http response body: " + body);
-	}
-
-}
