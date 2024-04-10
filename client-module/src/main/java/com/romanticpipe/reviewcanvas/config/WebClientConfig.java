ed448e0f2bef1099ad32aa77aa304af4a3dee41e
Hyeok_Choi <love72huk@naver.com>
2024-04-10 15:21:02 +0900

    feat(RVCV-121): webclient 로그를 남긴다

--- client-module/src/main/java/com/romanticpipe/reviewcanvas/config/WebClientConfig.java
+++ client-module/src/main/java/com/romanticpipe/reviewcanvas/config/WebClientConfig.java
@@ -1,6 +1,7 @@
 package com.romanticpipe.reviewcanvas.config;
 
 import io.netty.channel.ChannelOption;
+import io.netty.handler.logging.LogLevel;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
@@ -18,6 +20,7 @@ public class WebClientConfig {
 	@Bean
 	public WebClient cafe24WebClient() {
 		HttpClient httpClient = HttpClient.create()
+			.wiretap("reactor.netty.http.client.HttpClient", LogLevel.INFO, AdvancedByteBufFormat.TEXTUAL)
 			.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5_000)
 			.responseTimeout(Duration.ofMillis(5_000))
 			.doOnConnected(conn ->
