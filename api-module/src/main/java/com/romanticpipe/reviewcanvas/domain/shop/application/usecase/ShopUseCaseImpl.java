752429d91dfe2ba01f9afe4c3e16fa4617d882cf
Hyeok_Choi <love72huk@naver.com>
2024-04-12 19:16:30 +0900

    fix(RVCV-121): 만약 refresh token이 유효하지 않거나 만료되었을 경우, INVALID_OR_EXPIRED_REFRESH_TOKEN error를 발생시킨다.

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/shop/application/usecase/ShopUseCaseImpl.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/shop/application/usecase/ShopUseCaseImpl.java
@@ -1,12 +1,15 @@
 package com.romanticpipe.reviewcanvas.domain.shop.application.usecase;
 
+import com.romanticpipe.reviewcanvas.cafe24.Cafe24ErrorCode;
 import com.romanticpipe.reviewcanvas.cafe24.Cafe24FormUrlencodedFactory;
 import com.romanticpipe.reviewcanvas.cafe24.authentication.Cafe24AccessToken;
 import com.romanticpipe.reviewcanvas.cafe24.authentication.Cafe24AuthenticationClient;
 import com.romanticpipe.reviewcanvas.domain.shop.application.usecase.response.GetCafe24AccessTokenResponse;
+import com.romanticpipe.reviewcanvas.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Component
 @RequiredArgsConstructor
@@ -24,7 +27,14 @@ public class ShopUseCaseImpl implements ShopUseCase {
 	@Override
 	public GetCafe24AccessTokenResponse reissueCafe24AccessToken(String mallId, String refreshToken) {
 		MultiValueMap<String, String> requestParam = Cafe24FormUrlencodedFactory.reissueCafe24AccessToken(refreshToken);
-		Cafe24AccessToken cafe24AccessToken = cafe24AuthenticationClient.reissueAccessToken(mallId, requestParam);
-		return GetCafe24AccessTokenResponse.from(cafe24AccessToken);
+		try {
+			Cafe24AccessToken cafe24AccessToken = cafe24AuthenticationClient.reissueAccessToken(mallId, requestParam);
+			return GetCafe24AccessTokenResponse.from(cafe24AccessToken);
+		} catch (HttpClientErrorException ex) {
+			if (ex.getResponseBodyAsString().contains("\"error\":\"invalid_grant\"")) {
+				throw new BusinessException(Cafe24ErrorCode.INVALID_OR_EXPIRED_REFRESH_TOKEN);
+			}
+			throw ex;
+		}
 	}
 }
