e874afe2738991089899481327053b2e768c5cf9
Hyeok_Choi <love72huk@naver.com>
2024-04-12 19:14:02 +0900

    feat(RVCV-121): Cafe24 Error Code를 정의한다

---
+++ client-module/src/main/java/com/romanticpipe/reviewcanvas/cafe24/Cafe24ErrorCode.java
@@ -0,0 +1,29 @@
+package com.romanticpipe.reviewcanvas.cafe24;
+
+import com.romanticpipe.reviewcanvas.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Cafe24ErrorCode implements ErrorCode {
+
+	INVALID_OR_EXPIRED_REFRESH_TOKEN(400, "CA001", "cafe24 refresh token이 유효하지 않거나 만료되었습니다.");
+
+	private final int status;
+	private final String code;
+	private final String message;
+
+	@Override
+	public int getStatus() {
+		return status;
+	}
+
+	@Override
+	public String getCode() {
+		return code;
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+}
