83fcdfc1b31d5d0e2b6aebe10eab2789114db536
Hyeok_Choi <love72huk@naver.com>
2024-04-12 21:02:37 +0900

    feat: Controller 테스트 환경을 구축한다

---
+++ api-module/src/test/java/com/romanticpipe/reviewcanvas/config/ControllerTestSetup.java
@@ -0,0 +1,19 @@
+package com.romanticpipe.reviewcanvas.config;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+public abstract class ControllerTestSetup {
+
+	protected MockMvc noSecurityMockMvc;
+
+	@BeforeEach
+	void setUp(WebApplicationContext context) {
+		this.noSecurityMockMvc = MockMvcBuilders.webAppContextSetup(context)
+			.alwaysDo(MockMvcResultHandlers.print())
+			.build();
+	}
+}
