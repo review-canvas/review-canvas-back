be0df2811ef10eea42b81651110ca702a0ab095e
Hyeok_Choi <love72huk@naver.com>
2024-04-15 15:01:27 +0900

    fix(RVCV-98): getReviewVisibilityTitles 쿼리를 수정한다

--- domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/repository/ReviewVisibilityRepository.java
+++ domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/repository/ReviewVisibilityRepository.java
@@ -1,19 +1,18 @@
 package com.romanticpipe.reviewcanvas.repository;
 
-import java.util.List;
-
+import com.romanticpipe.reviewcanvas.domain.ReviewVisibility;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.romanticpipe.reviewcanvas.domain.ReviewVisibility;
+import java.util.List;
 
 public interface ReviewVisibilityRepository extends JpaRepository<ReviewVisibility, Long> {
 	@Query(
-		value = "SELECT COLUMN_NAME\n"
-			+ "FROM INFORMATION_SCHEMA.COLUMNS\n"
-			+ "WHERE TABLE_SCHEMA = 'review_canvas_db'\n"
-			+ "  AND TABLE_NAME = 'review_visibility'\n"
-			+ "  AND COLUMN_NAME NOT IN ('review_visibility_id')\n"
+		value = "SELECT COLUMN_NAME "
+			+ "FROM INFORMATION_SCHEMA.COLUMNS "
+			+ "WHERE TABLE_SCHEMA = 'review_canvas_db' "
+			+ "  AND TABLE_NAME = 'review_visibility' "
+			+ "  AND COLUMN_NAME NOT IN ('review_visibility_id') "
 			+ "ORDER BY ORDINAL_POSITION;",
 		nativeQuery = true
 	)
