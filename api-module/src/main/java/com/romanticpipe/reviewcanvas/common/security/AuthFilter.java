ed80a8d189e7c3588259a9f045c925fe4a6467ba
Hyeok_Choi <love72huk@naver.com>
2024-04-15 12:05:52 +0900

    refactor(RVCV-78): Token을 가져오는 로직을 리팩토링한다

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/common/security/AuthFilter.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/common/security/AuthFilter.java
@@ -1,16 +1,6 @@
 package com.romanticpipe.reviewcanvas.common.security;
 
-import static org.springframework.http.HttpHeaders.*;
-
-import java.io.IOException;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.util.StringUtils;
-import org.springframework.web.filter.OncePerRequestFilter;
-
 import com.romanticpipe.reviewcanvas.exception.BusinessException;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
@@ -20,6 +10,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @RequiredArgsConstructor
 public class AuthFilter extends OncePerRequestFilter {
@@ -30,15 +28,12 @@ public class AuthFilter extends OncePerRequestFilter {
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-		FilterChain filterChain) throws BusinessException, ServletException, IOException {
-		String jwt;
+									FilterChain filterChain) throws BusinessException, ServletException, IOException {
 		try {
-			jwt = resolveToken(request);
-			if (StringUtils.hasText(jwt)) {
-				Claims claims = tokenProvider.validateToken(jwt).getBody();
-				Authentication authentication = tokenProvider.getAuthentication(claims, jwt);
-				SecurityContextHolder.getContext().setAuthentication(authentication);
-			}
+			String jwt = findToken(request);
+			Claims claims = tokenProvider.validateToken(jwt).getBody();
+			Authentication authentication = tokenProvider.getAuthentication(claims, jwt);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
 		} catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
 			request.setAttribute(ATTRIBUTE_NAME, SecurityErrorCode.MAL_FORMED_TOKEN);
 		} catch (IllegalArgumentException e) {
@@ -47,13 +42,15 @@ public class AuthFilter extends OncePerRequestFilter {
 			request.setAttribute(ATTRIBUTE_NAME, SecurityErrorCode.EXPIRED_TOKEN);
 		} catch (UnsupportedJwtException e) {
 			request.setAttribute(ATTRIBUTE_NAME, SecurityErrorCode.UNSUPPORTED_TOKEN);
+		} catch (BusinessException e) {
+			request.setAttribute(ATTRIBUTE_NAME, e.getErrorCode());
 		} catch (Exception e) {
 			request.setAttribute(ATTRIBUTE_NAME, SecurityErrorCode.UNKNOWN_ERROR);
 		}
 		filterChain.doFilter(request, response);
 	}
 
-	private String resolveToken(HttpServletRequest request) {
+	private String findToken(HttpServletRequest request) {
 		String bearerToken = request.getHeader(AUTHORIZATION);
 		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
 			return bearerToken.substring(BEARER_PREFIX.length());
