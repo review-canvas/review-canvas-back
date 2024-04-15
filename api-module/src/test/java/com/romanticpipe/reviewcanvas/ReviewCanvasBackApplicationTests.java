048a7bcaf0a3bdd6ce1ad9e0397f60aa5345374a
JunseoHo <junseoho1029@gmail.com>
2024-04-15 13:45:31 +0900

    feat(RVCV-94) : 테스트 코드를 추가한다.

--- api-module/src/test/java/com/romanticpipe/reviewcanvas/ReviewCanvasBackApplicationTests.java
+++ api-module/src/test/java/com/romanticpipe/reviewcanvas/ReviewCanvasBackApplicationTests.java
@@ -1,13 +1,44 @@
 package com.romanticpipe.reviewcanvas;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.romanticpipe.reviewcanvas.domain.review.application.usecase.ReviewUseCase;
+import com.romanticpipe.reviewcanvas.dto.PageableRequest;
+import com.romanticpipe.reviewcanvas.enumeration.Direction;
+import com.romanticpipe.reviewcanvas.exception.BusinessException;
 
 @SpringBootTest
 class ReviewCanvasBackApplicationTests {
 
+	@Autowired
+	private ReviewUseCase reviewUseCase;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@DisplayName("특정 shop admin의 MyReviewDesign 리스트 조회 테스트")
+	@Test
+	void getMyReviewDesignsTest() {
+		PageableRequest pageableRequest = PageableRequest.of(0, 10, Direction.DESC);
+		long validShopAdminId0 = 1L;
+		long validShopAdminId1 = 2L;
+		long validShopAdminId2 = 3L;
+		long invalidShopAdminId0 = 0L;
+		long invalidShopAdminId1 = -1L;
+
+		assertDoesNotThrow(() -> reviewUseCase.getMyReviewDesigns(validShopAdminId0, pageableRequest));
+		assertDoesNotThrow(() -> reviewUseCase.getMyReviewDesigns(validShopAdminId1, pageableRequest));
+		assertDoesNotThrow(() -> reviewUseCase.getMyReviewDesigns(validShopAdminId2, pageableRequest));
+		assertThrows(BusinessException.class,
+			() -> reviewUseCase.getMyReviewDesigns(invalidShopAdminId0, pageableRequest));
+		assertThrows(BusinessException.class,
+			() -> reviewUseCase.getMyReviewDesigns(invalidShopAdminId1, pageableRequest));
 	}
 
 }
