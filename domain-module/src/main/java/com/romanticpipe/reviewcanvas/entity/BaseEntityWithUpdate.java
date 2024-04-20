bd9bae2ffd340171723ffb4b9265573d4f3c90e3
Hyeok_Choi <love72huk@naver.com>
2024-04-20 20:33:44 +0900

    fix: baseEntity에 notnull 어노테이션을 삭제한다

--- domain-module/src/main/java/com/romanticpipe/reviewcanvas/entity/BaseEntityWithUpdate.java
+++ domain-module/src/main/java/com/romanticpipe/reviewcanvas/entity/BaseEntityWithUpdate.java
@@ -3,7 +3,6 @@ package com.romanticpipe.reviewcanvas.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -15,7 +14,6 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntityWithUpdate extends BaseEntity {
 
-	@NotNull
 	@LastModifiedDate
 	@Column(nullable = false)
 	private LocalDateTime updatedAt;
