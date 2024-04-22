ee97eaefae46970cad5de9513059e9d1467dcc99
Hyeok_Choi <love72huk@naver.com>
2024-04-22 22:25:10 +0900

    feat(RVCV-71): security 테스트를위한 설정을 추가한다

--- api-module/src/test/java/com/romanticpipe/reviewcanvas/config/ControllerTestSetup.java
+++ api-module/src/test/java/com/romanticpipe/reviewcanvas/config/ControllerTestSetup.java
@@ -1,6 +1,7 @@
 package com.romanticpipe.reviewcanvas.config;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -9,10 +10,15 @@ import org.springframework.web.context.WebApplicationContext;
 public abstract class ControllerTestSetup {
 
 	protected MockMvc mockMvc;
+	protected MockMvc securityMockMvc;
 
 	@BeforeEach
 	void setUp(WebApplicationContext context) {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+			.alwaysDo(MockMvcResultHandlers.print())
+			.build();
+		this.securityMockMvc = MockMvcBuilders.webAppContextSetup(context)
+			.apply(SecurityMockMvcConfigurers.springSecurity())
 			.alwaysDo(MockMvcResultHandlers.print())
 			.build();
 	}
