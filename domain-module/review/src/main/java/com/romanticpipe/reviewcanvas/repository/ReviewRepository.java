86028c40889f6f7adb5d20b70c273aa0ae1f9a9a
gyuchangShim <shim17@mju.ac.kr>
2024-04-22 17:11:31 +0900

    fix(RVCV-73): 승인 대기 리뷰 조회 쿼리 변경

--- domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/repository/ReviewRepository.java
+++ domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/repository/ReviewRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.romanticpipe.reviewcanvas.domain.Review;
 
@@ -17,7 +18,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 	@Query("select r from Review r inner join Product p "
 		+ "on r.productId = p.id inner join ShopAdmin sa "
 		+ "on p.shopAdminId = sa.id "
-		+ "where sa.id = ?1 and r.status = 'WAITING'")
-	Page<Review> findAllByShopAdminId(long shopAdminId, Pageable pageable);
+		+ "where sa.id = :shopAdminId and r.status = 'WAITING'")
+	Page<Review> findAllByShopAdminId(@Param("shopAdminId") long shopAdminId, Pageable pageable);
 
 }
