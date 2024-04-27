a4a6abd810668bf2494fd997db0e40b313a1aa5d
Yoo Byeong gue <ybg653924@gmail.com>
2024-04-27 13:28:52 +0900

    feat(RVCV-98): my_review_design entity를 수정한다

--- domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/domain/MyReviewDesign.java
+++ domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/domain/MyReviewDesign.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,7 +18,14 @@ public class MyReviewDesign {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "my_review_design_id")
 	private Integer id;
-
-	private Integer reviewDesignId;
+	private Integer reviewListDesignId;
+	private Integer reviewModalDesignId;
 	private Integer shopAdminId;
+
+	@Builder
+	public MyReviewDesign(Integer reviewListDesignId, Integer reviewModalDesignId, Integer shopAdminId) {
+		this.reviewListDesignId = reviewListDesignId;
+		this.reviewModalDesignId = reviewModalDesignId;
+		this.shopAdminId = shopAdminId;
+	}
 }
