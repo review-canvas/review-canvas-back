17686c4e740043700f194060803f87ef05ee4b25
Yoo Byeong gue <ybg653924@gmail.com>
2024-04-30 12:46:30 +0900

    feat(RVCV-98): TermsService를 생성한다.

---
+++ domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/service/TermsService.java
@@ -0,0 +1,19 @@
+package com.romanticpipe.reviewcanvas.reviewproperty.service;
+
+import org.springframework.stereotype.Service;
+
+import com.romanticpipe.reviewcanvas.reviewproperty.domain.Terms;
+import com.romanticpipe.reviewcanvas.reviewproperty.exception.TermsNotFoundException;
+import com.romanticpipe.reviewcanvas.reviewproperty.repository.TermsRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TermsService {
+	private final TermsRepository termsRepository;
+
+	public Terms findByTag(String tag) {
+		return termsRepository.findByTag(tag).orElseThrow(TermsNotFoundException::new);
+	}
+}
