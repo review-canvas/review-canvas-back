b7791fa126b08daf187cbae0f297875601e1c558
Yoo Byeong gue <ybg653924@gmail.com>
2024-04-30 12:42:06 +0900

    fix(RVCV-98): TermsConsent entity를 수정한다.

--- domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/domain/TermsConsent.java
+++ domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/domain/TermsConsent.java
@@ -1,7 +1,13 @@
 package com.romanticpipe.reviewcanvas.reviewproperty.domain;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,6 +19,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class TermsConsent {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,6 +27,10 @@ public class TermsConsent {
 	private Integer id;
 	private Integer termsId;
 	private Boolean consent;
+
+	@CreatedDate
+	@Column(nullable = false)
+	private LocalDateTime consentAt;
 	private Integer shopAdminId;
 
 	@Builder
