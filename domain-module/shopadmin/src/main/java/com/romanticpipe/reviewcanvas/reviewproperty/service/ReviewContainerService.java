b43fabe019c8bd52728ff01940e70c9041708414
Hyeok_Choi <love72huk@naver.com>
2024-04-30 17:22:45 +0900

    fix: 대면 피드백에 따라 수정한다

--- domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/service/ReviewContainerService.java
+++ domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/service/ReviewContainerService.java
@@ -1,20 +1,18 @@
 package com.romanticpipe.reviewcanvas.reviewproperty.service;
 
-import org.springframework.stereotype.Service;
-
 import com.romanticpipe.reviewcanvas.exception.BusinessException;
 import com.romanticpipe.reviewcanvas.reviewproperty.domain.ReviewContainer;
 import com.romanticpipe.reviewcanvas.reviewproperty.exception.ReviewPropertyErrorCode;
 import com.romanticpipe.reviewcanvas.reviewproperty.repository.ReviewContainerRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewContainerService {
 	private final ReviewContainerRepository reviewContainerRepository;
 
-	public ReviewContainer findByShopAdminId(Integer shopAdminId) {
+	public ReviewContainer validateByShopAdminId(Integer shopAdminId) {
 		return reviewContainerRepository.findByShopAdminId(shopAdminId)
 			.orElseThrow(() -> new BusinessException(ReviewPropertyErrorCode.REVIEW_CONTAINER_NOT_FOUND));
 	}
