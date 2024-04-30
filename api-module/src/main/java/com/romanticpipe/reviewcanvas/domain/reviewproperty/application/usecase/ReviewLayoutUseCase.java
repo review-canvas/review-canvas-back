a7a79c1f79ff63f975b4161048c69af0252578af
gyuchangShim <shim17@mju.ac.kr>
2024-04-30 15:53:23 +0900

    feat(RVCV-159): review layout 조회 api 생성(api, controller, response)

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/reviewproperty/application/usecase/ReviewLayoutUseCase.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/reviewproperty/application/usecase/ReviewLayoutUseCase.java
@@ -1,7 +1,10 @@
 package com.romanticpipe.reviewcanvas.domain.reviewproperty.application.usecase;
 
 import com.romanticpipe.reviewcanvas.domain.reviewproperty.application.usecase.request.UpdateLayoutRequest;
+import com.romanticpipe.reviewcanvas.domain.reviewproperty.application.usecase.response.ReviewLayoutResponse;
 
 public interface ReviewLayoutUseCase {
 	void updateLayout(Integer adminId, UpdateLayoutRequest updateLayoutRequest);
+
+	ReviewLayoutResponse getReviewLayout();
 }
--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/reviewproperty/presentation/v1/ReviewLayoutApi.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/reviewproperty/presentation/v1/ReviewLayoutApi.java
@@ -4,11 +4,14 @@ import com.romanticpipe.reviewcanvas.common.dto.SuccessResponse;
 import com.romanticpipe.reviewcanvas.common.security.AuthInfo;
 import com.romanticpipe.reviewcanvas.common.security.JwtInfo;
 import com.romanticpipe.reviewcanvas.domain.reviewproperty.application.usecase.request.UpdateLayoutRequest;
+import com.romanticpipe.reviewcanvas.domain.reviewproperty.application.usecase.response.ReviewLayoutResponse;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -26,4 +29,14 @@ public interface ReviewLayoutApi {
 		@AuthInfo JwtInfo jwtInfo,
 		@RequestBody UpdateLayoutRequest updateLayoutRequest
 	);
+
+	@Operation(summary = "Layout 디자인 속성 조회 API", description = "Layout 디자인 속성을 조회한다.",
+		security = @SecurityRequirement(name = "Bearer Authentication"))
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "성공적으로 Layout 디자인 속성 조회를 완료했습니다.")
+	})
+	@GetMapping("/shop-admin/review-layout")
+	ResponseEntity<SuccessResponse<ReviewLayoutResponse>> getReviewLayout();
 }
