ff210729919ae98d7861bcf10011e8e481e35be9
Yoo Byeong gue <ybg653924@gmail.com>
2024-04-30 16:03:41 +0900

    feat(RVCV-161): Getter를 추가한다.

--- domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/domain/Padding.java
+++ domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/domain/Padding.java
@@ -3,9 +3,11 @@ package com.romanticpipe.reviewcanvas.reviewproperty.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Padding {
 
