bb1a966d7b7d8dc8a02af84d5acc9227234bce24
Gyulguma <ybg653924@gmail.com>
2024-05-02 16:49:30 +0900

    refactor(RVCV-161): 리뷰 Container 조회 api service를 수정한다.

--- domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/service/ReviewContainerService.java
+++ domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/service/ReviewContainerService.java
@@ -1,11 +1,12 @@
 package com.romanticpipe.reviewcanvas.reviewproperty.service;
 
-import com.romanticpipe.reviewcanvas.exception.BusinessException;
-import com.romanticpipe.reviewcanvas.reviewproperty.domain.ReviewContainer;
-import com.romanticpipe.reviewcanvas.reviewproperty.exception.ReviewPropertyErrorCode;
-import com.romanticpipe.reviewcanvas.reviewproperty.repository.ReviewContainerRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import com.romanticpipe.reviewcanvas.reviewproperty.domain.ReviewContainer;
+import com.romanticpipe.reviewcanvas.reviewproperty.exception.ReviewContainerNotFoundException;
+import com.romanticpipe.reviewcanvas.reviewproperty.repository.ReviewContainerRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +15,6 @@ public class ReviewContainerService {
 
 	public ReviewContainer validateByShopAdminId(Integer shopAdminId) {
 		return reviewContainerRepository.findByShopAdminId(shopAdminId)
-			.orElseThrow(() -> new BusinessException(ReviewPropertyErrorCode.REVIEW_CONTAINER_NOT_FOUND));
+			.orElseThrow(ReviewContainerNotFoundException::new);
 	}
 }
