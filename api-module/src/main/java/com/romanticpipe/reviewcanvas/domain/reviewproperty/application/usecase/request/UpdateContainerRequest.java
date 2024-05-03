dfb7e3dd72329e2a3be91a648e49a5efb990571d
woowal <jijile44@gmail.com>
2024-05-03 16:39:14 +0900

    fix(RVCV-154): HTTP Path, DTO Schema 수정

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/reviewproperty/application/usecase/request/UpdateContainerRequest.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/reviewproperty/application/usecase/request/UpdateContainerRequest.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-@Schema(name = "PostReviewContainerRequest", description = "Review Container 디자인 수정 요청")
+@Schema(name = "UpdateReviewContainerRequest", description = "Review Container 디자인 수정 요청")
 public record UpdateContainerRequest(@Schema(description = "가로", requiredMode = Schema.RequiredMode.REQUIRED)
 									 @NotBlank String width,
 									 @Schema(description = "내부여백", requiredMode = Schema.RequiredMode.REQUIRED)
