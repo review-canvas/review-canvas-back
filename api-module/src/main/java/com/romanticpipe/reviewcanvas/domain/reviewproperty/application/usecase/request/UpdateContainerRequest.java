a09d2a187e5dfd03088fa1ed911df9bfcf2d561c
Gyulguma <ybg653924@gmail.com>
2024-05-03 13:21:21 +0900

    refactor(RVCV-154): request를 수정한다.

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/reviewproperty/application/usecase/request/UpdateContainerRequest.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/reviewproperty/application/usecase/request/UpdateContainerRequest.java
@@ -5,16 +5,17 @@ import com.romanticpipe.reviewcanvas.reviewproperty.domain.Padding;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(name = "PostReviewContainerRequest", description = "Review Container 디자인 수정 요청")
 public record UpdateContainerRequest(@Schema(description = "가로", requiredMode = Schema.RequiredMode.REQUIRED)
 									 @NotBlank String width,
 									 @Schema(description = "내부여백", requiredMode = Schema.RequiredMode.REQUIRED)
-									 @NotBlank Padding padding,
+									 @NotNull Padding padding,
 									 @Schema(description = "배경", requiredMode = Schema.RequiredMode.REQUIRED)
 									 @NotBlank String background,
 									 @Schema(description = "테두리", requiredMode = Schema.RequiredMode.REQUIRED)
-									 @NotBlank Border border,
+									 @NotNull Border border,
 									 @Schema(description = "테두리 색상", requiredMode = Schema.RequiredMode.REQUIRED)
 									 @NotBlank String borderColor,
 									 @Schema(description = "그림자", requiredMode = Schema.RequiredMode.REQUIRED)
