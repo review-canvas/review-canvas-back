0397aaa16a4c462c3c0d7adebbb9eaafd944b229
Gyulguma <ybg653924@gmail.com>
2024-05-04 16:25:46 +0900

    feat(RVCV-98): 에러코드를 추가한다

--- domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/exception/TermsErrorCode.java
+++ domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/exception/TermsErrorCode.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum TermsErrorCode implements ErrorCode {
 
-	TERMS_ERROR_CODE(400, "T001", "약관 태그를 찾을 수 없습니다.");
+	TERMS_ERROR_CODE(400, "T001", "약관 태그를 찾을 수 없습니다."),
+	NOT_CONSENT_MANDATORY_TERMS(400, "T002", "필수 약관에 동의하지 않았습니다.");
 
 	private final int status;
 	private final String code;
