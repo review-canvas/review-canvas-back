e8c1762e176bd0165a1156d083240e83a6247db4
Hyeok_Choi <love72huk@naver.com>
2024-05-04 17:46:12 +0900

    refactor: 회원가입 usecase를 리팩토링한다

--- domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/repository/TermsRepository.java
+++ domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/repository/TermsRepository.java
@@ -1,14 +1,10 @@
 package com.romanticpipe.reviewcanvas.reviewproperty.repository;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.romanticpipe.reviewcanvas.reviewproperty.domain.Terms;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.romanticpipe.reviewcanvas.reviewproperty.domain.Terms;
+import java.util.List;
 
 public interface TermsRepository extends JpaRepository<Terms, Integer> {
-	Optional<Terms> findByTag(String tag);
-
 	List<Terms> findAllByMandatory(boolean isMandatory);
 }
