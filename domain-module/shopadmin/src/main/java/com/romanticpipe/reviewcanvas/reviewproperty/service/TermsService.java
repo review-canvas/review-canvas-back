97abf217e8ee9dc7e4836420a5af456f8058f5e6
Gyulguma <ybg653924@gmail.com>
2024-05-04 16:24:31 +0900

    feat(RVCV-98): service validate를 추가한다.

--- domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/service/TermsService.java
+++ domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/service/TermsService.java
@@ -1,8 +1,12 @@
 package com.romanticpipe.reviewcanvas.reviewproperty.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.romanticpipe.reviewcanvas.exception.BusinessException;
 import com.romanticpipe.reviewcanvas.reviewproperty.domain.Terms;
+import com.romanticpipe.reviewcanvas.reviewproperty.exception.TermsErrorCode;
 import com.romanticpipe.reviewcanvas.reviewproperty.exception.TermsNotFoundException;
 import com.romanticpipe.reviewcanvas.reviewproperty.repository.TermsRepository;
 
@@ -15,5 +19,13 @@ public class TermsService {
 
 	public Terms validateByTag(String tag) {
 		return termsRepository.findByTag(tag).orElseThrow(TermsNotFoundException::new);
+	}
+
+	public void validateMandatoryTerms(List<String> termsTags) {
+		List<Terms> mandatoryTermsList = termsRepository.findAllByMandatory(true);
+		for (Terms terms : mandatoryTermsList) {
+			if (!termsTags.contains(terms.getTag()))
+				throw new BusinessException(TermsErrorCode.NOT_CONSENT_MANDATORY_TERMS);
+		}
 	}
 }
