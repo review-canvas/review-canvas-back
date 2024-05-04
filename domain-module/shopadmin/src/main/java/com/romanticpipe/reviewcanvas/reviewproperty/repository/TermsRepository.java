978063f8a0835936a52534ac4c3c79b948c8878e
Gyulguma <ybg653924@gmail.com>
2024-05-04 16:25:38 +0900

    feat(RVCV-98): 레포지토리에 필수약관 찾기를 추가한다.

--- domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/repository/TermsRepository.java
+++ domain-module/shopadmin/src/main/java/com/romanticpipe/reviewcanvas/reviewproperty/repository/TermsRepository.java
@@ -1,5 +1,6 @@
 package com.romanticpipe.reviewcanvas.reviewproperty.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.romanticpipe.reviewcanvas.reviewproperty.domain.Terms;
 
 public interface TermsRepository extends JpaRepository<Terms, Integer> {
-	public Optional<Terms> findByTag(String tag);
+	Optional<Terms> findByTag(String tag);
+
+	List<Terms> findAllByMandatory(boolean isMandatory);
 }
