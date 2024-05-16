cc611acc4b4204e0b370d71bb542e62ba78c6acc
Gyulguma <ybg653924@gmail.com>
2024-05-16 22:08:24 +0900

    feat(RVCV-129): 댓글 생성 api의 request를 생성한다

---
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/request/CreateReplyRequest.java
@@ -0,0 +1,14 @@
+package com.romanticpipe.reviewcanvas.domain.review.application.usecase.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
+@Schema(name = "CreateReplyRequest", description = "댓글 생성 요청")
+public record CreateReplyRequest(
+	@Schema(description = "댓글을 작성한 유저의 member id", requiredMode = Schema.RequiredMode.REQUIRED)
+	@NotNull String memberId,
+	@Schema(description = "댓글 내용", requiredMode = Schema.RequiredMode.REQUIRED)
+	@NotNull String content
+) {
+
+}
