35a1d202a48c2627e5c86f625f9453e6eea620ec
Gyulguma <ybg653924@gmail.com>
2024-05-16 22:08:08 +0900

    feat(RVCV-129): ReplyApi에 댓글 생성 api를 생성한다.

---
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReplyApi.java
@@ -0,0 +1,32 @@
+package com.romanticpipe.reviewcanvas.domain.review.presentation.v1;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.romanticpipe.reviewcanvas.common.dto.SuccessResponse;
+import com.romanticpipe.reviewcanvas.domain.review.application.usecase.request.CreateReplyRequest;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Reply", description = "댓글 API")
+public interface ReplyApi {
+
+	@Operation(summary = "리뷰 댓글 생성 API", description = "특정 리뷰의 댓글을 생성한다.")
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "성공적으로 댓글 생성이 완료되었습니다.")
+	})
+	@PostMapping("/shop/{mallId}/products/{productNo}/reviews/{reviewId}/reply")
+	ResponseEntity<SuccessResponse<Void>> createReplyForUser(
+		@PathVariable("mallId") String mallId,
+		@PathVariable("productNo") Long productNo,
+		@PathVariable("reviewId") Long reviewId,
+		@RequestBody CreateReplyRequest createReplyRequest
+	);
+}
