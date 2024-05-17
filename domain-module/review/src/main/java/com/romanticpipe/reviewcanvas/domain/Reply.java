053033de15cd8a7e2c937496ee3af5b36977c0e3
Gyulguma <ybg653924@gmail.com>
2024-05-17 15:47:19 +0900

    feat(RVCV-129): Reply entity class를 수정한다

--- domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/domain/Reply.java
+++ domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/domain/Reply.java
@@ -1,12 +1,14 @@
 package com.romanticpipe.reviewcanvas.domain;
 
 import com.romanticpipe.reviewcanvas.entity.BaseEntityWithUpdate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,6 +25,7 @@ public class Reply extends BaseEntityWithUpdate {
 	private Long userId;
 	private String content;
 
+	@Builder
 	public Reply(Long reviewId, Long userId, String content) {
 		this.reviewId = reviewId;
 		this.userId = userId;
