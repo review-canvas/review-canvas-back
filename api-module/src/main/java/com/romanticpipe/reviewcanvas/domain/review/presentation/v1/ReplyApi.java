6fa2a263c5d3dd15795d0f47b9277b470a6525f3
Gyulguma <ybg653924@gmail.com>
2024-05-18 17:08:51 +0900

    feat(RVCV-36): 댓글 조회 api를 생성한다

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReplyApi.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReplyApi.java
@@ -1,12 +1,15 @@
 package com.romanticpipe.reviewcanvas.domain.review.presentation.v1;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.romanticpipe.reviewcanvas.common.dto.SuccessResponse;
 import com.romanticpipe.reviewcanvas.domain.review.application.usecase.request.CreateReplyRequest;
+import com.romanticpipe.reviewcanvas.domain.review.application.usecase.response.GetReplyForUserResponse;
+import com.romanticpipe.reviewcanvas.dto.PageResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -26,5 +29,16 @@ public interface ReplyApi {
 	ResponseEntity<SuccessResponse<Void>> createReplyForUser(
 		@PathVariable("reviewId") Long reviewId,
 		@RequestBody CreateReplyRequest createReplyRequest
+	);
+
+	@Operation(summary = "특정 리뷰의 댓글 조회 API", description = "특정 리뷰의 댓글들을 조회한다.")
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "성공적으로 댓글 조회가 완료되었습니다.")
+	})
+	@GetMapping("/reviews/{reviewId}/replies")
+	ResponseEntity<SuccessResponse<PageResponse<GetReplyForUserResponse>>> getReplyForUser(
+		@PathVariable("reviewId") Long reviewId
 	);
 }
