e930b2e656d659ab2e90a831fb16f79a65e1c979
Gyulguma <ybg653924@gmail.com>
2024-05-19 00:25:46 +0900

    feat(RVCV-35): 댓글 작성자 검증 로직을 추가한다

--- domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/service/ReplyService.java
+++ domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/service/ReplyService.java
@@ -1,8 +1,14 @@
 package com.romanticpipe.reviewcanvas.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.romanticpipe.reviewcanvas.domain.Reply;
+import com.romanticpipe.reviewcanvas.domain.User;
+import com.romanticpipe.reviewcanvas.exception.BusinessException;
+import com.romanticpipe.reviewcanvas.exception.ReplyErrorCode;
+import com.romanticpipe.reviewcanvas.exception.ReplyNotFoundException;
 import com.romanticpipe.reviewcanvas.repository.ReplyRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,8 +17,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ReplyService {
 	private final ReplyRepository replyRepository;
+	private final UserService userService;
 
 	public Reply save(Reply reply) {
 		return replyRepository.save(reply);
+	}
+
+	public Reply findById(Long replyId) {
+		return replyRepository.findById(replyId).orElseThrow(ReplyNotFoundException::new);
+	}
+
+	public void validateUserIsWriter(Reply reply, Optional<User> optionalUser) {
+		optionalUser.ifPresentOrElse(
+			user -> {
+				if (user.getId() != reply.getUserId())
+					throw new BusinessException(ReplyErrorCode.WRITER_NOT_MATCH);
+			}, () -> {
+				throw new BusinessException(ReplyErrorCode.WRITER_NOT_MATCH);
+			}
+		);
 	}
 }
