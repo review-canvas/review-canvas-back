2eaed916d5deec08580a89520e11ac0b68e147c8
Yoo Byeong gue <ybg653924@gmail.com>
2024-05-20 14:47:57 +0900

    refactor(RVCV-129): createReplyForUser 코드 수정

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/ReplyUseCaseImpl.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/ReplyUseCaseImpl.java
@@ -1,16 +1,16 @@
 package com.romanticpipe.reviewcanvas.domain.review.application.usecase;
 
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionTemplate;
+
 import com.romanticpipe.reviewcanvas.domain.Reply;
 import com.romanticpipe.reviewcanvas.domain.User;
 import com.romanticpipe.reviewcanvas.domain.review.application.usecase.request.CreateReplyRequest;
 import com.romanticpipe.reviewcanvas.service.ReplyService;
 import com.romanticpipe.reviewcanvas.service.ReviewService;
 import com.romanticpipe.reviewcanvas.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.support.TransactionTemplate;
 
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
@@ -25,11 +25,16 @@ public class ReplyUseCaseImpl implements ReplyUseCase {
 
 	@Override
 	public void createReplyForUser(Long reviewId, CreateReplyRequest createReplyRequest) {
-		Optional<User> optionalUser = readTransactionTemplate.execute(status -> {
-			reviewService.validById(reviewId);
-			return userService.findUser(createReplyRequest.memberId(), createReplyRequest.mallId());
-		});
-		User user = optionalUser.orElseGet(
+		//TODO: sonarCloud 정적분석 통과 안된 코드
+		// Optional<User> optionalUser = readTransactionTemplate.execute(status -> {
+		// 	reviewService.validById(reviewId);
+		// 	return userService.findUser(createReplyRequest.memberId(), createReplyRequest.mallId());
+		// });
+		// User user = optionalUser.orElseGet(
+		// 	() -> userUseCase.createSaveUser(createReplyRequest.mallId(), createReplyRequest.memberId())
+		// );
+
+		User user = userService.findUser(createReplyRequest.memberId(), createReplyRequest.mallId()).orElseGet(
 			() -> userUseCase.createSaveUser(createReplyRequest.mallId(), createReplyRequest.memberId())
 		);
 
