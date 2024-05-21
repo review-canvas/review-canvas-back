c33baafd3a5259e2804e746c2a714b21c84e5a69
Yoo Byeong gue <ybg653924@gmail.com>
2024-05-21 17:17:11 +0900

    fix(RVCV-35): error code를 수정한다

--- domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/exception/ReplyNotFoundException.java
+++ domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/exception/ReplyNotFoundException.java
@@ -2,6 +2,6 @@ package com.romanticpipe.reviewcanvas.exception;
 
 public class ReplyNotFoundException extends BusinessException {
 	public ReplyNotFoundException() {
-		super(ReplyErrorCode.REPLY_NOT_FOUND);
+		super(ReviewErrorCode.REPLY_NOT_FOUND);
 	}
 }
--- domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/exception/ReviewErrorCode.java
+++ domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/exception/ReviewErrorCode.java
@@ -9,8 +9,15 @@ public enum ReviewErrorCode implements ErrorCode {
 	REVIEW_NOT_FOUND(400, "R001", "리뷰를 찾을 수 없습니다."),
 
 	// Product
-	PRODUCT_NOT_FOUND(400, "R002", "상품을 찾을 수 없습니다.");
+	PRODUCT_NOT_FOUND(400, "R002", "상품을 찾을 수 없습니다."),
 
+	// User
+	USER_NOT_FOUND(400, "R003", "유저를 찾을 수 없습니다."),
+
+	// Reply
+	REPLY_NOT_FOUND(400, "R003", "댓글을 찾을 수 없습니다."),
+	WRITER_NOT_MATCH(400, "R004", "댓글의 작성자가 아닙니다."),
+	REPLY_CAN_NOT_UPDATE(400, "R005", "댓글을 수정할 수 없습니다");
 	private final int status;
 	private final String code;
 	private final String message;
