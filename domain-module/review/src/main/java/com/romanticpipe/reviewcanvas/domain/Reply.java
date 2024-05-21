580a9489c0d6b43fa0237f1c7d86a25b6be71dd5
Yoo Byeong gue <ybg653924@gmail.com>
2024-05-21 15:33:19 +0900

    fix(RVCV-129): reply에 column 추가에 따라 수정한다

--- domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/domain/Reply.java
+++ domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/domain/Reply.java
@@ -25,11 +25,13 @@ public class Reply extends BaseEntityWithUpdate {
 	@Column(name = "users_id")
 	private Long userId;
 	private String content;
+	private Boolean deleted;
 
 	@Builder
-	public Reply(Long reviewId, Long userId, String content) {
+	public Reply(Long reviewId, Long userId, String content, Boolean deleted) {
 		this.reviewId = reviewId;
 		this.userId = userId;
 		this.content = content;
+		this.deleted = deleted;
 	}
 }
