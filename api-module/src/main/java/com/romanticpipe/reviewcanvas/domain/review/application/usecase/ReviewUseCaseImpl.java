a5ccbb03f108a3a84d6ccecc596a1c426b2c6268
gyuchangShim <shim17@mju.ac.kr>
2024-05-21 18:20:16 +0900

    style(RVCV-123): checkstyle 수정

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/ReviewUseCaseImpl.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/ReviewUseCaseImpl.java
@@ -77,7 +77,6 @@ class ReviewUseCaseImpl implements ReviewUseCase {
 		review.setScore(updateReviewRequest.score());
 		review.setContent(updateReviewRequest.content());
 
-		// TODO 공통 코드인데 private 메서드로 빼는 것이 좋을지?
 		Product product = productService.findProduct(review.getProductId())
 			.orElseThrow(() -> new BusinessException(ReviewErrorCode.PRODUCT_NOT_FOUND));
 		String dirPath = "public-view/shop-admin" + product.getShopAdminId() + "/product-" + review.getProductId();
