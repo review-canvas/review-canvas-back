5a151ac5820e7dd2ddfd589e5b1db9ce8403f178
Yoo Byeong gue <ybg653924@gmail.com>
2024-05-21 16:43:19 +0900

    fix(RVCV-36): error code를 수정한다

--- domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/exception/UserNotFoundException.java
+++ domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/exception/UserNotFoundException.java
@@ -2,6 +2,6 @@ package com.romanticpipe.reviewcanvas.exception;
 
 public class UserNotFoundException extends BusinessException {
 	public UserNotFoundException() {
-		super(UserErrorCode.USER_NOT_FOUND);
+		super(ReviewErrorCode.USER_NOT_FOUND);
 	}
 }
