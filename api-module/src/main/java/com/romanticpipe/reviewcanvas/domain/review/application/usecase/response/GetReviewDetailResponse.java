fe6b75c3f57b7960cc91688ec5b127e9aec2e465
woowal <jijile44@gmail.com>
2024-05-23 16:34:42 +0900

    feat(RVCV-53): 상품별 리뷰 조회 시 본인 리뷰인지 확인한다

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/response/GetReviewDetailResponse.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/response/GetReviewDetailResponse.java
@@ -1,10 +1,11 @@
 package com.romanticpipe.reviewcanvas.domain.review.application.usecase.response;
 
+import java.util.List;
+
 import com.romanticpipe.reviewcanvas.domain.Review;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
-
-import java.util.List;
 
 @Builder
 @Schema(name = "GetReviewDetailResponse", description = "리뷰 및 댓글 조회 response")
@@ -15,21 +16,24 @@ public record GetReviewDetailResponse(
 	String content,
 	@Schema(description = "리뷰 점수", requiredMode = Schema.RequiredMode.REQUIRED)
 	Integer score,
-	@Schema(description = "리뷰 작성자 id", requiredMode = Schema.RequiredMode.REQUIRED)
-	Long userId,
+	@Schema(description = "리뷰 작성자 cafe24 id", requiredMode = Schema.RequiredMode.REQUIRED)
+	String memberId,
 	@Schema(description = "리뷰 작성자 닉네임", requiredMode = Schema.RequiredMode.REQUIRED)
 	String nickname,
+	@Schema(description = "본인 작성 리뷰 여부", requiredMode = Schema.RequiredMode.REQUIRED)
+	Boolean isMine,
 	@Schema(description = "리뷰에 해당하는 댓글 리스트", requiredMode = Schema.RequiredMode.REQUIRED)
 	List<ReplyResponse> replies
 ) {
 
-	public static GetReviewDetailResponse from(Review review) {
+	public static GetReviewDetailResponse from(Review review, boolean isMine) {
 		return GetReviewDetailResponse.builder()
 			.reviewId(review.getId())
 			.content(review.getContent())
 			.score(review.getScore())
-			.userId(review.getUser().getId())
+			.memberId(review.getUser().getMemberId())
 			.nickname(review.getUser().getNickName())
+			.isMine(isMine)
 			.replies(review.getReplyList().stream().map(ReplyResponse::from).toList())
 			.build();
 	}
