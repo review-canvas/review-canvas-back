3038f7377d2f8003f84374d2589d944cb524444b
Hyeok_Choi <love72huk@naver.com>
2024-05-27 11:30:32 +0900

    fix: 암호화를 위해 User 엔티티를 업데이트한다

--- domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/domain/User.java
+++ domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/domain/User.java
@@ -1,19 +1,18 @@
 package com.romanticpipe.reviewcanvas.domain;
 
-import java.time.LocalDate;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Table(name = "users")
 @Entity
@@ -21,30 +20,49 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
+	@Transient
+	private final UserEncryptor userEncryptor = new UserEncryptor();
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "users_id")
 	private Long id;
-
 	private String memberId;
 	private String mallId;
 	private String name;
 	private String nickName;
-	@Enumerated(EnumType.STRING)
-	@Column(columnDefinition = "VARCHAR")
-	private Gender gender;
+	private String gender;
 	private String nationality;
-	private LocalDate birth;
+	private String birth;
 
 	@Builder
 	public User(String memberId, String mallId, String name, String nickName, Gender gender, String nationality,
-		LocalDate birth) {
+				LocalDate birth) {
 		this.memberId = memberId;
 		this.mallId = mallId;
-		this.name = name;
-		this.nickName = nickName;
-		this.gender = gender;
-		this.nationality = nationality;
-		this.birth = birth;
+		this.name = userEncryptor.encrypt(name);
+		this.nickName = userEncryptor.encrypt(nickName);
+		this.gender = userEncryptor.encrypt(gender.name());
+		this.nationality = userEncryptor.encrypt(nationality);
+		this.birth = userEncryptor.encrypt(birth.toString());
+	}
+
+	public String getName() {
+		return userEncryptor.decrypt(name);
+	}
+
+	public String getNickName() {
+		return userEncryptor.decrypt(nickName);
+	}
+
+	public Gender getGender() {
+		return Gender.valueOf(userEncryptor.decrypt(gender));
+	}
+
+	public String getNationality() {
+		return userEncryptor.decrypt(nationality);
+	}
+
+	public LocalDate getBirth() {
+		return LocalDate.parse(userEncryptor.decrypt(birth));
 	}
 }
