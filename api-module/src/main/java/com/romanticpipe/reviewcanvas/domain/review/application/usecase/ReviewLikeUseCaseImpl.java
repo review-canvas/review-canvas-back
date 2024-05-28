a0fb890990bb8c178d3271df409ec1a882939e0f
Yoo Byeong gue <ybg653924@gmail.com>
2024-05-28 20:36:36 +0900

    feat(RVCV-141): 리뷰ID 검증 로직을 추가한다

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/ReviewLikeUseCaseImpl.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/ReviewLikeUseCaseImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.romanticpipe.reviewcanvas.service.ReviewLikeService;
+import com.romanticpipe.reviewcanvas.service.ReviewService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,10 +13,12 @@ import lombok.RequiredArgsConstructor;
 public class ReviewLikeUseCaseImpl implements ReviewLikeUseCase {
 
 	private final ReviewLikeService reviewLikeService;
+	private final ReviewService reviewService;
 
 	@Override
 	@Transactional(readOnly = true)
 	public int getReviewLikeCount(Long reviewId) {
+		reviewService.validById(reviewId);
 		return reviewLikeService.getReviewLikeCount(reviewId);
 	}
 }
