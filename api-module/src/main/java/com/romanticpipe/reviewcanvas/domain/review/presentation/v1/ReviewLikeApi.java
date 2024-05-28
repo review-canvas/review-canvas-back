124ee84d19f11cfe83c21db9ce6d5a179ed1881f
Yoo Byeong gue <ybg653924@gmail.com>
2024-05-28 20:56:37 +0900

    feat(RVCV-142): 좋아요 생성 api를 생성한다

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReviewLikeApi.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReviewLikeApi.java
@@ -1,8 +1,30 @@
 package com.romanticpipe.reviewcanvas.domain.review.presentation.v1;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.romanticpipe.reviewcanvas.common.dto.SuccessResponse;
+import com.romanticpipe.reviewcanvas.domain.review.application.usecase.request.CreateReviewLikeRequest;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "ReviewLike", description = "리뷰 좋아요 API")
 public interface ReviewLikeApi {
 
+	@Operation(summary = "리뷰 좋아요 생성(User) API", description = "User가 특정 리뷰의 좋아요를 누른다.")
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "성공적으로 리뷰 좋아요 생성(User)이 완료되었습니다.")
+	})
+	@PostMapping("/reviews/{reviewId}/like/")
+	ResponseEntity<SuccessResponse<Void>> createReviewLike(
+		@PathVariable("reviewId") Long reviewId,
+		@RequestBody CreateReviewLikeRequest createReviewLikeRequest
+	);
 }
--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReviewLikeController.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReviewLikeController.java
@@ -1,9 +1,13 @@
 package com.romanticpipe.reviewcanvas.domain.review.presentation.v1;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.romanticpipe.reviewcanvas.common.dto.SuccessResponse;
 import com.romanticpipe.reviewcanvas.domain.review.application.usecase.ReviewLikeUseCase;
+import com.romanticpipe.reviewcanvas.domain.review.application.usecase.request.CreateReviewLikeRequest;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,4 +18,10 @@ public class ReviewLikeController implements ReviewLikeApi {
 
 	private final ReviewLikeUseCase reviewLikeUseCase;
 
+	@Override
+	public ResponseEntity<SuccessResponse<Void>> createReviewLike(Long reviewId,
+		CreateReviewLikeRequest createReviewLikeRequest) {
+
+		return SuccessResponse.ofNoData().asHttp(HttpStatus.OK);
+	}
 }
