013985e3de29e093b28b83cddca98a3ab448de50
Yoo Byeong gue <ybg653924@gmail.com>
2024-05-29 15:52:01 +0900

    feat(RVCV-143): (User) delete usecaseImpl을 구현한다

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/ReviewLikeUseCaseImpl.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/application/usecase/ReviewLikeUseCaseImpl.java
@@ -2,8 +2,11 @@ package com.romanticpipe.reviewcanvas.domain.review.application.usecase;
 
 import org.springframework.stereotype.Component;
 
+import com.romanticpipe.reviewcanvas.domain.Review;
+import com.romanticpipe.reviewcanvas.domain.User;
 import com.romanticpipe.reviewcanvas.service.ReviewLikeService;
 import com.romanticpipe.reviewcanvas.service.ReviewService;
+import com.romanticpipe.reviewcanvas.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,9 +16,12 @@ public class ReviewLikeUseCaseImpl implements ReviewLikeUseCase {
 
 	private final ReviewLikeService reviewLikeService;
 	private final ReviewService reviewService;
+	private final UserService userService;
 
 	@Override
 	public void deleteReviewLike(String mallId, String memberId, long reviewId) {
-		
+		User user = userService.validByMemberIdAndMallId(memberId, mallId);
+		Review review = reviewService.validById(reviewId);
+		reviewLikeService.deleteReviewLike(user.getId(), review.getId(), null);
 	}
 }
