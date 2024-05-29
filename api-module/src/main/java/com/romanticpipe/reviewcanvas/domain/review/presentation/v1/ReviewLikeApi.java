56da01b2dc5de67e42c1e9900d5624f841536fdc
Yoo Byeong gue <ybg653924@gmail.com>
2024-05-29 15:18:50 +0900

    feat(RVCV-143): (User) 좋아요 취소 api를 생성한다

--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReviewLikeApi.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReviewLikeApi.java
@@ -1,8 +1,29 @@
 package com.romanticpipe.reviewcanvas.domain.review.presentation.v1;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.romanticpipe.reviewcanvas.common.dto.SuccessResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "ReviewLike", description = "리뷰 좋아요 API")
 public interface ReviewLikeApi {
 
+	@Operation(summary = "리뷰 좋아요 취소(User) API", description = "User가 특정 리뷰에 좋아요를 취소한다.")
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "성공적으로 리뷰 좋아요 취소(User)가 완료되었습니다.")
+	})
+	@DeleteMapping("/shop/{mallId}/users/{memberId}/reviews/{reviewId}/like")
+	ResponseEntity<SuccessResponse<Void>> deleteReviewLike(
+		@PathVariable("mallId") String mallId,
+		@PathVariable("memberId") String memberId,
+		@PathVariable("reviewId") long reviewId
+	);
 }
--- api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReviewLikeController.java
+++ api-module/src/main/java/com/romanticpipe/reviewcanvas/domain/review/presentation/v1/ReviewLikeController.java
@@ -1,8 +1,12 @@
 package com.romanticpipe.reviewcanvas.domain.review.presentation.v1;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.romanticpipe.reviewcanvas.common.dto.SuccessResponse;
 import com.romanticpipe.reviewcanvas.domain.review.application.usecase.ReviewLikeUseCase;
 
 import lombok.RequiredArgsConstructor;
@@ -14,4 +18,13 @@ public class ReviewLikeController implements ReviewLikeApi {
 
 	private final ReviewLikeUseCase reviewLikeUseCase;
 
+	@Override
+	@DeleteMapping("/shop/{mallId}/users/{memberId}/reviews/{reviewId}/like")
+	public ResponseEntity<SuccessResponse<Void>> deleteReviewLike(
+		@PathVariable("mallId") String mallId,
+		@PathVariable("memberId") String memberId,
+		@PathVariable("reviewId") long reviewId
+	) {
+		return null;
+	}
 }
