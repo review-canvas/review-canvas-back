4d77dd317b40a492c8fe8531525872bb0f6c0ffe
Hyeok_Choi <love72huk@naver.com>
2024-06-15 19:41:22 +0900

    fix: 필요없는 생성자를 삭제한다

--- client-module/src/main/java/com/romanticpipe/reviewcanvas/cafe24/product/Cafe24ProductsDto.java
+++ client-module/src/main/java/com/romanticpipe/reviewcanvas/cafe24/product/Cafe24ProductsDto.java
@@ -1,12 +1,8 @@
 package com.romanticpipe.reviewcanvas.cafe24.product;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
 
 import java.util.List;
 
 public record Cafe24ProductsDto(@Getter List<Cafe24Product> products) {
-	@JsonCreator
-	public Cafe24ProductsDto {
-	}
 }
