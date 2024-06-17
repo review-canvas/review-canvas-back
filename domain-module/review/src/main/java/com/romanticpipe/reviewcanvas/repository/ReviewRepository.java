85ac58a0845df4d2e44e263139a1a4ebd55ac13d
Hyeok_Choi <love72huk@naver.com>
2024-06-17 15:52:41 +0900

    fix: shop admin 조회 api 쿼리를 수정한다

--- domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/repository/ReviewRepository.java
+++ domain-module/review/src/main/java/com/romanticpipe/reviewcanvas/repository/ReviewRepository.java
@@ -1,11 +1,10 @@
 package com.romanticpipe.reviewcanvas.repository;
 
-import java.util.Optional;
-
+import com.romanticpipe.reviewcanvas.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.romanticpipe.reviewcanvas.domain.Review;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewQueryRepository {
 
@@ -14,6 +13,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewQue
 	Optional<Review> findByIdAndUserId(Long reviewId, Long userId);
 
 	@Query("select count(r) from Review r join r.product p join ShopAdmin sa on p.shopAdminId = sa.id"
-		+ " where sa.id = :shopAdminId and r.deletedAt = null ")
+		+ " where sa.id = :shopAdminId and r.deletedAt is null ")
 	Long countByShopAdminId(Integer shopAdminId);
 }
